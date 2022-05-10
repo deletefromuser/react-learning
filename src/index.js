4ee67e397d0c769171d2148e0a678cd5bd9172bc
zhang <mrzhangbin@outlook.com>
2022-05-10 23:52:19 +0800

    ReactRouter example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './index.css';
-import Game from './TutorialChess/Game';
 import Bookkeeper from './Router/BookKeeper';
+import Expenses from './Router/Expenses';
+import Invoice from './Router/Invoice';
+import Invoices from "./Router/Invoices";
+import Game from './TutorialChess/Game';
 import Context from './TutorialOther/Context';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <>
-        <Game />
-        <hr className="my-3" />
-        <Context></Context>
-        <hr className="my-3" />
-        <Bookkeeper></Bookkeeper>
-        <hr className="my-3" />
-    </>);
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<>
+                <Game />
+                <hr className="my-3" />
+                <Context></Context>
+                <hr className="my-3" />
+
+                <Link to="/router">Bookkeeper - ReactRouter</Link>
+            </>} />
+            <Route path="router" element={<Bookkeeper />} >
+                <Route path="expenses" element={<Expenses />} />
+                <Route path="invoices" element={<Invoices />} >
+                    <Route path=":invoiceId" element={<Invoice />} />
+                </Route>
+                <Route path="*"
+                    element={
+                        <main style={{ padding: "1rem" }}>
+                            <p>There's nothing here!</p>
+                        </main>
+                    }
+                />
+            </Route>
+
+        </Routes>
+    </BrowserRouter>
+);

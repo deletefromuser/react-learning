4859352496b2317d97ca51fb2f89c9f7c08704fd
zhang <mrzhangbin@outlook.com>
2022-05-23 15:43:07 +0800

    follow the tutorial https://flexiple.com/react/introduction-to-higher-order-components-in-react-by-example

--- src/TutorialOther/Hoc.jsx
+++ src/TutorialOther/Hoc.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import { getUsers } from "../Router/data";
 
 export const UserCard = props => {
@@ -14,13 +13,14 @@ export const UserCard = props => {
             <p>
                 <b>Website:</b> {props.website}
             </p>
-            <hr />
         </div>
     );
 };
 
 export const UserList = (props) => {
-    let { data: users } = props;
+    let { data: users, searchKey } = props;
+    let UserCardWithBorder = withBorder(UserCard);
+    users = users.filter(user => user.name.toLowerCase().includes(searchKey.toLowerCase()));
     return (
         <div>
             <div>
@@ -29,7 +29,7 @@ export const UserList = (props) => {
                 </div>
             </div>
             <div>
-                {users.length > 0 && users.map((user) => <UserCard key={user.id} {...user} />)}
+                {users.length > 0 && users.map((user) => <UserCardWithBorder key={user.id} {...user} />)}
             </div>
         </div>
     )
@@ -44,13 +44,14 @@ export const UserAddressCard = props => {
             <p>
                 <b>Address:</b> {props.address.street} {props.address.suite}, {props.address.city}
             </p>
-            <hr />
         </div>
     );
 };
 
 export const UserAddressList = (props) => {
-    let { data: users } = props;
+    let { data: users, searchKey } = props;
+    let UserAddressCardWithBorder = withBorder(UserAddressCard);
+    users = users.filter(user => user.name.toLowerCase().includes(searchKey.toLowerCase()));
     return (
         <div>
             <div>
@@ -59,11 +60,57 @@ export const UserAddressList = (props) => {
                 </div>
             </div>
             <div>
-                {users.length > 0 && users.map((user) => <UserAddressCard key={user.id} {...user} />)}
+                {users.length > 0 && users.map((user) => <UserAddressCardWithBorder key={user.id} {...user} />)}
             </div>
         </div>
     )
 }
+
+const withBorder = WrappedComponent => {
+    class WithBorder extends React.Component {
+        render() {
+            return (<div className="border" >
+                <WrappedComponent {...this.props}></WrappedComponent>
+            </div>)
+        }
+    }
+    WithBorder.displayName = `WithBorder(${getDisplayName(WrappedComponent)})`;
+    return WithBorder;
+}
+
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+const withSearch = WrappedComponent => {
+    class WithSearch extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                searchKey: ""
+            }
+        }
+
+        handleChange = event => {
+            this.setState({ searchKey: event.target.value });
+        }
+
+        render() {
+            return (<>
+                <div className="d-flex justify-content-center align-item-center my-2">
+                    <input value={this.state.searchKey} onChange={this.handleChange}></input>
+                </div>
+
+                <WrappedComponent searchKey={this.state.searchKey} {...this.props}></WrappedComponent>
+            </>)
+        }
+    }
+    WithSearch.displayName = `WithBorder(${getDisplayName(WrappedComponent)})`;
+    return WithSearch;
+}
+
+const UserListWithSearch = withSearch(UserList);
+const UserAddressListWithSearch = withSearch(UserAddressList);
 
 export default function HOC() {
     const [users, setUsers] = useState([]);
@@ -78,22 +125,17 @@ export default function HOC() {
     }, [])
 
     return (
-        <div style={{ display: "flex" }}>
-            <nav
-                style={{
-                    borderRight: "solid 1px",
-                    padding: "1rem",
-                }}
-            >
-                {users.length === 0 && <p>Loading...</p>}
-                {users.length > 0 &&
-                    <>
-                        <UserList data={users}></UserList>
-                        <hr></hr>
-                        <UserAddressList data={users}></UserAddressList>
-                    </>}
-            </nav>
-            <Outlet />
+        <div className="d-flex flex-row">
+            {users.length === 0 && <p>Loading...</p>}
+            {users.length > 0 &&
+                <>
+                    <div className="flex-grow-1">
+                        <UserListWithSearch data={users}></UserListWithSearch>
+                    </div>
+                    <div className="flex-grow-1">
+                        <UserAddressListWithSearch data={users}></UserAddressListWithSearch>
+                    </div>
+                </>}
         </div>
     );
 }
